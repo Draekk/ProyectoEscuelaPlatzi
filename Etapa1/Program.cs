2fd83637e1a2f6fb5d37d9ea7c9cc947fe005925
Draekk <rodriver1992@gmail.com>
2023-02-23 19:54:20 -0300

    Creacion de clase Escuela y su Constructor

---
+++ Etapa1/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using ProyectoEscuelaPlatzi.Entidades;
+
+var escuela = new Escuela("Platzi Academy", 2012);
+escuela.Pais = "Chile";
+escuela.Ciudad = "Curacaví";
+
+Console.WriteLine(escuela.Nombre);
