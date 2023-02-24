879a4a665f1cc70e57f97620e8bd56f39489ecde
Draekk <rodriver1992@gmail.com>
2023-02-23 21:00:51 -0300

    Creacion de clase TiposDeEscuela, override de ToString() y muestreo de los atributos de el objeto Escuela

--- Etapa1/Entidades/Escuela.cs
+++ Etapa1/Entidades/Escuela.cs
@@ -4,7 +4,7 @@ namespace ProyectoEscuelaPlatzi.Entidades
     {
         private string nombre;
         public string Nombre{
-            get{return "Copia: " + nombre;}
+            get{return nombre;}
             set{nombre = value.ToUpper();}
         }
 
@@ -12,7 +12,13 @@ namespace ProyectoEscuelaPlatzi.Entidades
 
         public string Pais { get; set; }
         public string Ciudad { get; set; }
+        public TiposEscuela tiposEscuela { get; set; }
 
         public Escuela(string nombre, int año) => (Nombre, AñoDeCreacion) = (nombre, año);
+
+        public override string ToString()
+        {
+            return $"Nombre: {Nombre}, Tipo: {tiposEscuela}\nPais: {Pais}, Ciudad: {Ciudad}";
+        }
     }
 }
--- Etapa1/Program.cs
+++ Etapa1/Program.cs
@@ -4,5 +4,5 @@ using ProyectoEscuelaPlatzi.Entidades;
 var escuela = new Escuela("Platzi Academy", 2012);
 escuela.Pais = "Chile";
 escuela.Ciudad = "Curacaví";
-
-Console.WriteLine(escuela.Nombre);
+escuela.tiposEscuela = TiposEscuela.Primaria;
+Console.WriteLine(escuela);
